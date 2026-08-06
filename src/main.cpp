62f24d12ec6834501ff6fd2d5002e05d494bee75
Torin <torinnkeenan@gmail.com>
2026-08-06 15:10:27 -0400

    Added 4 letter word file

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+int main(){
+    //Get start word
+    std::cout<<"Enter starting word:";
+    std::string start_word;
+    std::cin>>start_word;
+    
+    //Get end word
+    std::cout<<"Enter ending word";
+    std::string end_word;
+    std::cin>>end_word;
+
+
+
+
+
+
+
+}
